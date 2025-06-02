95ba08a5b7b23d0cb79a49d83f8e1507c4a83098
Eseo0 <2seo0eo@gmail.com>
2025-06-02 12:07:27 +0900

    update naver login ui

--- Web/config/urls.py
+++ Web/config/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Home.urls')),
+    path('main/', include('Home.urls')),
     path('user/', include('User.urls')),
     path('chatbot/', include('Chatbot.urls')),
 ]
+
